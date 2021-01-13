ac6887a34e6b292c5e2ca906c7cc18f12b9fe3b5
João Fassina <jpfassina@live.com>
2021-01-13 09:20:57 -0300

    Add files via upload

---
+++ src/gradienteCon.jl
@@ -0,0 +1,61 @@
+export gradienteCon
+function gradienteCon(nlp ; max_time = 10, max_iter = 100_000, η₁ = 1e-2, atol = 1e-6 ,rtol = 1e-6)
+    t₀ = time()
+    Δt = time() - t₀
+    iter = 0
+    
+    x = copy(nlp.meta.x0)
+    f(x) = obj(nlp,x)
+    ∇f(x) = grad(nlp,x)
+    fx = f(x)
+    gx = ∇f(x)
+    
+    x⁺ = similar(x)
+    
+    ϵ = atol + rtol * norm(gx)
+    
+    status =:unknown
+    
+    resolvido = norm(gx) < ϵ
+    cansado = Δt > max_time || iter > max_iter
+    while !(cansado || resolvido)
+        d = -gx
+        slope = dot(gx,d)
+        
+        α = 1.0
+        x⁺ = x + α * d
+        f⁺ = f(x⁺)
+        while f⁺ ≥ fx + η₁ * α * slope
+            α = α / 2
+            x⁺ = x + α * d
+            f⁺ = f(x⁺)
+            if α < 1e-8
+                status = :small_step
+                break
+            end
+        end
+        if status != :unknown
+            break
+        end
+        x .= x⁺
+        fx = f⁺
+        gx = ∇f(x)
+        
+        resolvido = norm(gx) < ϵ
+        Δt = time() - t₀
+        iter += 1
+        cansado = Δt > max_time || iter > max_iter
+    end
+    
+    if resolvido
+        status = :first_order
+    elseif cansado
+        if Δt >max_time
+            status = :max_time
+        elseif iter > max_iter
+            status = :max_iter
+        end
+    end
+    
+    return GenericExecutionStats(status,nlp,objective=fx,solution=x,dual_feas=norm(gx),iter = iter, elapsed_time = Δt)
+end
